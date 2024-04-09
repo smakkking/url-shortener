756547f538ad5922efe5b021bfffc518c10da2c4
andreysm <smirnov.andrey79@wildberries.work>
2024-04-09 12:25:37 +0300

    инит

---
+++ cmd/service/service.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// init логгер
+
+	// загрузка конфига
+
+	// init репозитории
+
+	// init сервисы
+
+	// init хендлеры
+
+	// запуск сервера
+
+	// graceful shutdown
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smakkking/url-shortener
+
+go 1.22.2
